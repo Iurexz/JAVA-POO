7f44a697cf23b5ce90cc23e50a8b6815a72b89f1
Iure Costa <131163902+Iurexz@users.noreply.github.com>
2025-02-13 00:35:30 +0000

    Aula 1

---
+++ poo/aula.java
@@ -0,0 +1,16 @@
+package poo;
+
+
+import models.Pessoa;
+
+public class aula {
+    public static void main(String[] args) {
+         
+    Pessoa pessoa = new Pessoa()
+        
+        
+       
+
+        
+}
+}
